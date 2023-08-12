10765312a32ed9c935381d156024593875eb500e
Bugguga <nat14660@hotmail.com>
2023-08-12 15:47:01 +0700

    Create Two_Sum.py

---
+++ easy/Two_Sum.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        nums.sort()
+        seen = {}
+        for i in range(len(nums)):
+            diff = target - nums[i]
+            if diff in seen:
+                return [seen[diff], i]
+            else:
+                seen[nums[i]] = i
+
+nums = [int(x) for x in input().split()]
+target = int(input())
+sol = Solution()
+ans = sol.twoSum(nums,target)
+print(ans)
